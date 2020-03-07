a2d18b2b699bfed2fac2a350ba10c3c72e44af3f
CzarqR <czarekr98@gmail.com>
2020-03-07 14:51:06 +0100

    Init

---
+++ Calculus.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculus
+    {
+        public static bool CalculaceCalculator(ref string inEq, ref string formatedEQ)
+        {
+            if (NormalToReverse(ref inEq, ref formatedEQ))
+            {
+                if (Calculate(ref inEq))
+                {
+                    return true;
+                }
+                return false;
+
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+        private static bool NormalToReverse(ref string n, ref string reformatedStr)
+        {
+            n = n.Replace("()", "");
+            if (n.Length == 0)
+            {
+                n = "Equation is in wrong format";
+                return false;
+            }
+            if (n.Length > 1)
+            {
+                n = AddSpacesAfterOperator(n);
+            }
+            if (!ValidateCharacters(n))
+            {
+                n = "Equation contains invalid character";
+                return false;
+            }
+            if (!IsEqBracketsCorrect(n))
+            {
+                n = "Brackets in equation are placed wrong";
+                return false;
+            }
+            if (n[0].Equals('+'))
+            {
+                n = n.Remove(0, 2);
+            }
+            else if (n[0].Equals('-'))
+            {
+                n = n.Remove(1, 1);
+            }
+            n = n.Replace("( +", "( 0 +");
+            n = n.Replace("( -", "( 0 -");
+            reformatedStr = n;
+            string[] s = n.Split(' ');
+            if (!IsEqCorrect(s))
+            {
+                n = "Equation is in wrong format";
+                return false;
+            }
+
+            Stack<char> stack = new Stack<char>();
+            StringBuilder r = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (double.TryParse(s[i], NumberStyles.Any, new CultureInfo("pl"), out _)) //number
+                {
+                    r.Append(s[i] + " ");
+                }
+                else //operator
+                {
+                    if (stack.Count == 0) //stack is empty
+                    {
+                        stack.Push(char.Parse(s[i]));
+                    }
+                    else //stack not empty
+                    {
+                        if (s[i].Equals(")")) //right bracket
+                        {
+                            while (!stack.Peek().Equals('('))
+                            {
+                                r.Append(stack.Pop() + " ");
+                            }
+                            stack.Pop();
+                        }
+                        else if (s[i].Equals("(")) //left bracket
+                        {
+                            stack.Push('(');
+                        }
+                        else
+                        {
+                            while (stack.Count > 0 && weight[stack.Peek()] >= weight[char.Parse(s[i])])
+                            {
+                                r.Append(stack.Pop() + " ");
+                            }
+                            stack.Push(char.Parse(s[i]));
+                        }
+                    }
+                }
+            }
+            while (stack.Count > 0)
+            {
+                r.Append(stack.Pop() + " ");
+            }
+            n = r.ToString().Trim();
+            return true;
+        }
+
+        private static bool Calculate(ref string r)
+        {
+            Stack<double> stack = new Stack<double>();
+            string[] s = r.Split(' ');
+            Console.WriteLine(string.Join("|", s.ToArray()));
+            double a, b;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (double.TryParse(s[i], NumberStyles.Any, new CultureInfo("pl"), out double c)) //number
+                {
+                    stack.Push(c);
+                }
+                else //operator
+                {
+                    b = stack.Pop();
+                    a = stack.Pop();
+                    switch (char.Parse(s[i]))
+                    {
+                        case '+':
+                            stack.Push(a + b);
+                            break;
+                        case '-':
+                            stack.Push(a - b);
+                            break;
+                        case '*':
+                            stack.Push(a * b);
+                            break;
+                        case '/':
+                            if (b == 0)
+                            {
+                                r = "You can't divide by 0";
+                                return false;
+                            }
+                            stack.Push(a / b);
+                            break;
+                        case '^':
+                            stack.Push(Math.Pow(a, b));
+                            break;
+                        case '%':
+                            if (b == 0)
+                            {
+                                r = "You can't divide by 0";
+                                return false;
+                            }
+                            stack.Push(a % b);
+                            break;
+                        default:
+                            r = "Equation contains invalid character";
+                            return false;
+
+                    }
+
+                }
+
+            }
+            r = string.Format("{0:F99}", stack.Pop()).TrimEnd('0').TrimEnd(',');
+            return true;
+        }
+        private static bool IsEqBracketsCorrect(string x)
+        {
+            int s = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i].Equals('('))
+                {
+                    s++;
+                }
+                else if (x[i].Equals(')'))
+                {
+                    s--;
+                }
+                if (s < 0)
+                {
+                    return false;
+                }
+            }
+            if (s != 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        private static bool IsEqCorrect(string[] x, int min = 0, int max = 1)
+        {
+
+            int s = 0;
+            foreach (string p in x)
+            {
+                if (double.TryParse(p, NumberStyles.Any, new CultureInfo("pl"), out _)) //number
+                {
+                    s++;
+                }
+                else if (p.Equals("(") || p.Equals(")")) //bracket
+                {
+                    continue;
+                }
+                else //operator
+                {
+                    s--;
+                }
+
+                if (s < min || s > max)
+                {
+                    return false;
+                }
+
+            }
+            if (s != 1)
+            {
+                return false;
+            }
+            return true;
+        }
+        private static bool ValidateCharacters(string x)
+        {
+            Regex rgx = new Regex(@"^[0-9+/*% \-^(),]+$"); //every operator and number must be separated by a space
+            return rgx.IsMatch(x);
+        }
+        private static string AddSpacesAfterOperator(string x)
+        {
+            StringBuilder s = new StringBuilder();
+            switch (x[0])
+            {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '%':
+                case '(':
+                case ')':
+                case '^':
+                    if (!x[1].Equals(' '))
+                    {
+                        s.Append(x[0] + " ");
+                    }
+                    else
+                    {
+                        s.Append(x[0]);
+                    }
+                    break;
+                default:
+                    s.Append(x[0]);
+                    break;
+            }
+            int max = x.Length - 1;
+            for (int i = 1; i < max; i++)
+            {
+                switch (x[i])
+                {
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                    case '%':
+                    case '(':
+                    case ')':
+                    case '^':
+                        if (!s[s.Length - 1].Equals(' '))
+                        {
+                            s.Append(" " + x[i]);
+                        }
+                        else
+                        {
+                            s.Append(x[i]);
+                        }
+                        if (!x[i + 1].Equals(' '))
+                        {
+                            s.Append(" ");
+                        }
+                        break;
+                    default:
+                        s.Append(x[i]);
+                        break;
+                }
+            }
+            switch (s[s.Length - 1])
+            {
+                case ' ':
+                    s.Append(x[x.Length - 1]);
+                    break;
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '%':
+                case '(':
+                case ')':
+                case '^':
+                    s.Append(" " + x[x.Length - 1]);
+                    break;
+                default: //number
+                    if (x[x.Length - 1].Equals(')'))
+                    {
+                        s.Append(" " + x[x.Length - 1]);
+                    }
+                    else
+                    {
+                        s.Append(x[x.Length - 1]);
+                    }
+                    break;
+            }
+            return s.ToString();
+        }
+
+        private static readonly Dictionary<char, byte> weight = new Dictionary<char, byte>
+        {
+                { '(', 0 },
+                { ')', 1 },
+                { '+', 1 },
+                { '-', 1 },
+                { '*', 2 },
+                { '/', 2 },
+                { '%', 2 },
+                { '^', 3 }
+        };
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+/// <summary>
+/// CO JEST ŹLE
+/// Global resources do ktorych mozna sie odwolywac zamiast kolorów w kodzie w kazdym miejscu
+/// przekazywanie stringow do funkcji z klasy Calculus jest głupie, mozna pewnie zrobic to lepiej
+/// tekst gdzie jest rownanie mogbły sie zawijać i wyrównywać do prawej lub zmieniac rozmiar a label nie powinien  sie rozszerzac bardziej niz rozmiar okna
+/// </summary>
+
+namespace Calculator
+{
+    public partial class MainWindow : Window
+    {
+        private bool toClear = true;
+        public const int buttonClickShowTime = 165;//ms 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateText(string s)
+        {
+            if (toClear) //lbl start operation or after invalid eq
+            {
+                lblCurr.Content = s;
+                toClear = false;
+            }
+            else
+            {
+                lblCurr.Content += s;
+            }
+        }
+
+        private void Calculate()
+        {
+            string currEq = lblCurr.Content.ToString();
+            string formatedEQ = string.Empty, result = currEq;
+            if (currEq.Length > 1)
+            {
+                if (Calculus.CalculaceCalculator(ref result, ref formatedEQ)) //OK
+                {
+                    lblCurr.Content = result;
+                    lblEq.Content = formatedEQ;
+                    brdEq.BorderBrush = new SolidColorBrush(Color.FromRgb(239, 201, 175));
+
+                }
+                else //WRONG
+                {
+                    brdEq.BorderBrush = Brushes.Red;
+                    toClear = false;
+                    lblCurr.Content = currEq;
+                    lblEq.Content = result;
+                }
+
+            }
+            else if (currEq.Length == 1)
+            {
+                Regex regex = new Regex(@"\d");
+                if (regex.IsMatch(currEq))
+                {
+
+                    lblCurr.Content = result;
+                    brdEq.BorderBrush = new SolidColorBrush(Color.FromRgb(239, 201, 175));
+                    lblEq.Content = currEq;
+                }
+            }
+        }
+
+        private void Del()
+        {
+            if (lblCurr.Content.ToString().Length != 0)
+            {
+                lblCurr.Content = lblCurr.Content.ToString().Substring(0, lblCurr.Content.ToString().Length - 1);
+            }
+        }
+
+        private void ShowClick(char x)
+        {
+            string c = x.ToString();
+            foreach (Button btn in grdButtons.Children)
+            {
+                if (btn.Content.Equals(c))
+                {
+                    BtnClick(btn);
+                    break;
+                }
+
+            }
+        }
+
+        private async void BtnClick(Button btn)
+        {
+            btn.Background = new SolidColorBrush(Color.FromRgb(16, 76, 145));
+            await Task.Delay(buttonClickShowTime);
+            btn.Background = new SolidColorBrush(Color.FromRgb(31, 138, 192));
+        }
+
+
+        private void BtnTextInput(object sender, RoutedEventArgs e)
+        {
+            UpdateText((sender as Button).Content.ToString());
+
+        }
+
+        private void BtnEquals(object sender, RoutedEventArgs e)
+        {
+            Calculate();
+        }
+
+        private void BtnClear(object sender, RoutedEventArgs e)
+        {
+            lblCurr.Content = string.Empty;
+        }
+
+        private void BtnDel(object sender, RoutedEventArgs e)
+        {
+            Del();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Back)
+            {
+                BtnClick(btnDel);
+                Del();
+            }
+            if (e.Key == Key.Enter)
+            {
+                ShowClick('=');
+                Calculate();
+            }
+        }
+
+        private void Window_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            char keyChar = (char)Encoding.ASCII.GetBytes(e.Text)[0];
+
+
+            if ("0123456789,%*^/+-()".Contains(keyChar)) //operator or number
+            {
+                ShowClick(keyChar);
+                UpdateText(keyChar.ToString());
+            }
+            else if (keyChar.Equals('c') || keyChar.Equals('C')) //C - clear
+            {
+                BtnClick(btnClear);
+                lblCurr.Content = string.Empty;
+            }
+            else if (keyChar.Equals('='))
+            {
+                Calculate();
+            }
+        }
+    }
+}
